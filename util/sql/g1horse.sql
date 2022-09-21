396fe824d5bace29ace9aae1d000523255d66f2d
u-10bei <u10bei.github.io@gmail.com>
2022-09-21 10:47:37 +0900

    add sql

--- util/sql/g1horse.sql
+++ util/sql/g1horse.sql
@@ -1,207 +1,248 @@
--- --------------------------------------------------------
--- ホスト:                          127.0.0.1
--- サーバーのバージョン:                   10.9.2-MariaDB - mariadb.org binary distribution
--- サーバー OS:                      Win64
--- HeidiSQL バージョン:               11.3.0.6295
--- --------------------------------------------------------
+-- phpMyAdmin SQL Dump
+-- version 4.4.15.10
+-- https://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: 2022 年 9 月 21 日 10:45
+-- サーバのバージョン： 10.5.15-MariaDB-log
+-- PHP Version: 5.4.16
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET NAMES utf8 */;
-/*!50503 SET NAMES utf8mb4 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
---  ビュー xs386482_g1horse.forgroup の構造をダンプしています
--- VIEW 依存エラーを克服するために、一時テーブルを作成
-CREATE TABLE `forgroup` (
-	`FatherId` SMALLINT(6) NULL,
-	`Horse` VARCHAR(20) NOT NULL COLLATE 'utf8mb4_general_ci',
-	`Child` BIGINT(21) NOT NULL
-) ENGINE=MyISAM;
+--
+-- Database: `xs386482_g1horse`
+--
 
---  ビュー xs386482_g1horse.forlink の構造をダンプしています
--- VIEW 依存エラーを克服するために、一時テーブルを作成
-CREATE TABLE `forlink` (
-	`FatherId` SMALLINT(6) NULL,
-	`Child` BIGINT(21) NOT NULL
-) ENGINE=MyISAM;
+-- --------------------------------------------------------
 
---  ビュー xs386482_g1horse.fornode の構造をダンプしています
--- VIEW 依存エラーを克服するために、一時テーブルを作成
-CREATE TABLE `fornode` (
-	`Id` SMALLINT(6) NOT NULL,
-	`Horse` VARCHAR(20) NOT NULL COLLATE 'utf8mb4_general_ci',
-	`FatherId` SMALLINT(6) NULL,
-	`GroupId` SMALLINT(6) NULL,
-	`GroupName` VARCHAR(20) NULL COLLATE 'utf8mb4_general_ci',
-	`Power` BIGINT(22) NOT NULL
-) ENGINE=MyISAM;
+--
+-- ビュー用の代替構造 `ForGroup`
+--
+CREATE TABLE IF NOT EXISTS `ForGroup` (
+`FatherId` smallint(6)
+,`Horse` varchar(20)
+,`Child` bigint(21)
+);
 
---  ビュー xs386482_g1horse.fornode2 の構造をダンプしています
--- VIEW 依存エラーを克服するために、一時テーブルを作成
-CREATE TABLE `fornode2` (
-	`Id` SMALLINT(6) NULL,
-	`GroupName` VARCHAR(20) NULL COLLATE 'utf8mb4_general_ci'
-) ENGINE=MyISAM;
+-- --------------------------------------------------------
 
---  ビュー xs386482_g1horse.forpower の構造をダンプしています
--- VIEW 依存エラーを克服するために、一時テーブルを作成
-CREATE TABLE `forpower` (
-	`WinnerId` SMALLINT(6) NOT NULL,
-	`Win` BIGINT(21) NOT NULL
-) ENGINE=MyISAM;
+--
+-- ビュー用の代替構造 `ForLink`
+--
+CREATE TABLE IF NOT EXISTS `ForLink` (
+`FatherId` smallint(6)
+,`Child` bigint(21)
+);
 
---  ビュー xs386482_g1horse.forstratify の構造をダンプしています
--- VIEW 依存エラーを克服するために、一時テーブルを作成
-CREATE TABLE `forstratify` (
-	`from` VARCHAR(20) NOT NULL COLLATE 'utf8mb4_general_ci',
-	`to` VARCHAR(20) NOT NULL COLLATE 'utf8mb4_general_ci',
-	`value` BIGINT(22) NOT NULL
-) ENGINE=MyISAM;
+-- --------------------------------------------------------
 
---  ビュー xs386482_g1horse.link の構造をダンプしています
--- VIEW 依存エラーを克服するために、一時テーブルを作成
-CREATE TABLE `link` (
-	`from` VARCHAR(20) NOT NULL COLLATE 'utf8mb4_general_ci',
-	`to` VARCHAR(20) NOT NULL COLLATE 'utf8mb4_general_ci',
-	`value` BIGINT(22) NOT NULL
-) ENGINE=MyISAM;
+--
+-- ビュー用の代替構造 `ForNode`
+--
+CREATE TABLE IF NOT EXISTS `ForNode` (
+`Id` smallint(6)
+,`Horse` varchar(20)
+,`FatherId` smallint(6)
+,`GroupId` smallint(6)
+,`GroupName` varchar(20)
+,`Power` bigint(22)
+);
 
---  ビュー xs386482_g1horse.node の構造をダンプしています
--- VIEW 依存エラーを克服するために、一時テーブルを作成
-CREATE TABLE `node` (
-	`id` VARCHAR(20) NULL COLLATE 'utf8mb4_general_ci',
-	`label` VARCHAR(20) NULL COLLATE 'utf8mb4_general_ci',
-	`group` VARCHAR(20) NULL COLLATE 'utf8mb4_general_ci',
-	`value` BIGINT(23) NOT NULL
-) ENGINE=MyISAM;
+-- --------------------------------------------------------
 
---  テーブル xs386482_g1horse.pedigree の構造をダンプしています
-CREATE TABLE IF NOT EXISTS `pedigree` (
+--
+-- ビュー用の代替構造 `ForNode2`
+--
+CREATE TABLE IF NOT EXISTS `ForNode2` (
+`Id` smallint(6)
+,`GroupName` varchar(20)
+);
+
+-- --------------------------------------------------------
+
+--
+-- ビュー用の代替構造 `ForPower`
+--
+CREATE TABLE IF NOT EXISTS `ForPower` (
+`WinnerId` smallint(6)
+,`Win` bigint(21)
+);
+
+-- --------------------------------------------------------
+
+--
+-- ビュー用の代替構造 `ForStratify`
+--
+CREATE TABLE IF NOT EXISTS `ForStratify` (
+`From` varchar(20)
+,`To` varchar(20)
+,`Value` bigint(22)
+);
+
+-- --------------------------------------------------------
+
+--
+-- ビュー用の代替構造 `Link`
+--
+CREATE TABLE IF NOT EXISTS `Link` (
+`From` varchar(20)
+,`To` varchar(20)
+,`Value` bigint(22)
+);
+
+-- --------------------------------------------------------
+
+--
+-- ビュー用の代替構造 `Node`
+--
+CREATE TABLE IF NOT EXISTS `Node` (
+`Id` varchar(20)
+,`Label` varchar(20)
+,`Group` varchar(20)
+,`Value` bigint(23)
+);
+
+-- --------------------------------------------------------
+
+--
+-- テーブルの構造 `Pedigree`
+--
+
+CREATE TABLE IF NOT EXISTS `Pedigree` (
   `Id` smallint(6) NOT NULL,
   `Horse` varchar(20) NOT NULL,
-  `FatherId` smallint(6) DEFAULT NULL,
-  PRIMARY KEY (`Id`),
-  KEY `FatherId` (`FatherId`)
+  `FatherId` smallint(6) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
--- エクスポートするデータが選択されていません
+-- --------------------------------------------------------
 
---  テーブル xs386482_g1horse.race_winner の構造をダンプしています
-CREATE TABLE IF NOT EXISTS `race_winner` (
+--
+-- テーブルの構造 `Race_Winner`
+--
+
+CREATE TABLE IF NOT EXISTS `Race_Winner` (
   `Ryear` smallint(6) NOT NULL,
   `Rtitle` varchar(50) NOT NULL,
-  `WinnerId` smallint(6) NOT NULL,
-  PRIMARY KEY (`Ryear`,`Rtitle`)
+  `WinnerId` smallint(6) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
--- エクスポートするデータが選択されていません
+-- --------------------------------------------------------
 
---  ビュー xs386482_g1horse.stratifylink の構造をダンプしています
--- VIEW 依存エラーを克服するために、一時テーブルを作成
-CREATE TABLE `stratifylink` (
-	`from` VARCHAR(20) NOT NULL COLLATE 'utf8mb4_general_ci',
-	`to` VARCHAR(20) NOT NULL COLLATE 'utf8mb4_general_ci',
-	`value` BIGINT(22) NOT NULL,
-	`Power` BIGINT(22) NOT NULL
-) ENGINE=MyISAM;
+--
+-- ビュー用の代替構造 `StratifyLink`
+--
+CREATE TABLE IF NOT EXISTS `StratifyLink` (
+`From` varchar(20)
+,`To` varchar(20)
+,`Value` bigint(22)
+,`Power` bigint(22)
+);
 
---  ビュー xs386482_g1horse.forgroup の構造をダンプしています
--- 一時テーブルを削除して、最終的な VIEW 構造を作成
-DROP TABLE IF EXISTS `forgroup`;
-CREATE ALGORITHM=UNDEFINED SQL SECURITY DEFINER VIEW `forgroup` AS SELECT ForLink.FatherId, Pedigree.Horse, ForLink.Child
-FROM ForLink INNER JOIN Pedigree ON ForLink.FatherId = Pedigree.Id
-WHERE ((ForLink.Child)>1) 
-ORDER BY ForLink.Child DESC, ForLink.FatherId ASC ;
+-- --------------------------------------------------------
 
---  ビュー xs386482_g1horse.forlink の構造をダンプしています
--- 一時テーブルを削除して、最終的な VIEW 構造を作成
-DROP TABLE IF EXISTS `forlink`;
-CREATE ALGORITHM=UNDEFINED SQL SECURITY DEFINER VIEW `forlink` AS SELECT Pedigree.FatherId, Count(Pedigree.FatherId) AS Child
-FROM Pedigree
-GROUP BY Pedigree.FatherId
-HAVING (((Pedigree.FatherId)>0)) ;
+--
+-- ビュー用の構造 `ForGroup`
+--
+DROP TABLE IF EXISTS `ForGroup`;
 
---  ビュー xs386482_g1horse.fornode の構造をダンプしています
--- 一時テーブルを削除して、最終的な VIEW 構造を作成
-DROP TABLE IF EXISTS `fornode`;
-CREATE ALGORITHM=UNDEFINED SQL SECURITY DEFINER VIEW `fornode` AS SELECT
-	Pedigree.Id,
-	Pedigree.Horse,
-	Pedigree.FatherId,
-	ForGroup.FatherId AS GroupId,
-	ForGroup.Horse AS GroupName,
-	ifnull(ForGroup.Child, 0) + 1 AS Power
-FROM Pedigree LEFT JOIN ForGroup ON Pedigree.Id = ForGroup.FatherId
-WHERE Pedigree.FatherId IS NOT NULL ;
+CREATE ALGORITHM=UNDEFINED DEFINER=`xs386482_u10bei`@`localhost` SQL SECURITY DEFINER VIEW `ForGroup` AS select `ForLink`.`FatherId` AS `FatherId`,`Pedigree`.`Horse` AS `Horse`,`ForLink`.`Child` AS `Child` from (`ForLink` join `Pedigree` on(`ForLink`.`FatherId` = `Pedigree`.`Id`)) where `ForLink`.`Child` > 1;
 
---  ビュー xs386482_g1horse.fornode2 の構造をダンプしています
--- 一時テーブルを削除して、最終的な VIEW 構造を作成
-DROP TABLE IF EXISTS `fornode2`;
-CREATE ALGORITHM=UNDEFINED SQL SECURITY DEFINER VIEW `fornode2` AS WITH RECURSIVE ancestor (Depth, Id, FatherId, GroupName, Power) AS (
-      SELECT 0, ForNode.Id, ForNode.FatherId, ForNode.GroupName, ForNode.Power FROM ForNode 
-			WHERE (ForNode.Id = 18 OR ForNode.Id = 133 OR ForNode.Id = 157)
-		UNION ALL
-        SELECT 
-            ancestor.Depth + 1,
-            ForNode.Id,
-            ForNode.FatherId,
-            CASE
-            	WHEN ForNode.Power = 1 THEN  ancestor.GroupName
-            	WHEN ancestor.Power - 1 > ForNode.Power THEN ancestor.GroupName
-            	ELSE ForNode.GroupName
-				END,
-            CASE
-					WHEN ancestor.Power - 1 > ForNode.Power THEN ancestor.Power - 1
-            	ELSE ForNode.Power
-				END			
-        FROM ancestor, ForNode
-        WHERE ancestor.Id = ForNode.FatherId)
-SELECT Id, GroupName FROM ancestor ORDER BY Id ;
+-- --------------------------------------------------------
 
---  ビュー xs386482_g1horse.forpower の構造をダンプしています
--- 一時テーブルを削除して、最終的な VIEW 構造を作成
-DROP TABLE IF EXISTS `forpower`;
-CREATE ALGORITHM=UNDEFINED SQL SECURITY DEFINER VIEW `forpower` AS SELECT Race_Winner.WinnerId, Count(Race_Winner.Rtitle) AS Win
-FROM Race_Winner
-GROUP BY Race_Winner.WinnerId ;
+--
+-- ビュー用の構造 `ForLink`
+--
+DROP TABLE IF EXISTS `ForLink`;
 
---  ビュー xs386482_g1horse.forstratify の構造をダンプしています
--- 一時テーブルを削除して、最終的な VIEW 構造を作成
-DROP TABLE IF EXISTS `forstratify`;
-CREATE ALGORITHM=UNDEFINED SQL SECURITY DEFINER VIEW `forstratify` AS SELECT Pedigree.Horse AS `from`,
-	case
-	 when Pedigree.Id = 0 then ''
-	 else Pedigree_1.Horse
-	end AS `to`,
-	IFNULL( ForPower.Win,0 ) + 1 AS `value`
-from ((Pedigree join Pedigree AS Pedigree_1 on(Pedigree.FatherId = Pedigree_1.Id))
- left join ForPower on(Pedigree.Id = ForPower.WinnerId)) ;
+CREATE ALGORITHM=UNDEFINED DEFINER=`xs386482_u10bei`@`localhost` SQL SECURITY DEFINER VIEW `ForLink` AS select `Pedigree`.`FatherId` AS `FatherId`,count(`Pedigree`.`FatherId`) AS `Child` from `Pedigree` group by `Pedigree`.`FatherId` having `Pedigree`.`FatherId` > 0;
 
---  ビュー xs386482_g1horse.link の構造をダンプしています
--- 一時テーブルを削除して、最終的な VIEW 構造を作成
-DROP TABLE IF EXISTS `link`;
-CREATE ALGORITHM=UNDEFINED SQL SECURITY DEFINER VIEW `link` AS SELECT Pedigree.Horse AS 'from', Pedigree_1.Horse AS 'to', ifnull(ForPower.Win,0)+1 AS 'value'
-FROM (Pedigree INNER JOIN Pedigree AS Pedigree_1 ON Pedigree.FatherId = Pedigree_1.Id) LEFT OUTER JOIN ForPower ON Pedigree.Id = ForPower.WinnerId
-WHERE (Pedigree.Id >0 AND Pedigree.FatherId >0) ;
+-- --------------------------------------------------------
 
---  ビュー xs386482_g1horse.node の構造をダンプしています
--- 一時テーブルを削除して、最終的な VIEW 構造を作成
-DROP TABLE IF EXISTS `node`;
-CREATE ALGORITHM=UNDEFINED SQL SECURITY DEFINER VIEW `node` AS SELECT ForNode.Horse AS id, ForNode.Horse AS label, ForNode2.GroupName AS 'group', ifnull(ForNode.Power,0)+ifnull(ForPower.Win,0) AS 'value'
-FROM (ForNode2 LEFT OUTER JOIN ForNode ON ForNode2.Id = ForNode.Id) LEFT OUTER JOIN ForPower ON ForNode2.Id = ForPower.WinnerId ;
+--
+-- ビュー用の構造 `ForNode`
+--
+DROP TABLE IF EXISTS `ForNode`;
 
---  ビュー xs386482_g1horse.stratifylink の構造をダンプしています
--- 一時テーブルを削除して、最終的な VIEW 構造を作成
-DROP TABLE IF EXISTS `stratifylink`;
-CREATE ALGORITHM=UNDEFINED SQL SECURITY DEFINER VIEW `stratifylink` AS SELECT ForStratify.`from`,
-	ForStratify.`to`,
-	ForStratify.value,
-	fornode.Power
-from (ForStratify join ForNode on(ForStratify.`from` = fornode.Horse)) ;
+CREATE ALGORITHM=UNDEFINED DEFINER=`xs386482_u10bei`@`localhost` SQL SECURITY DEFINER VIEW `ForNode` AS select `Pedigree`.`Id` AS `Id`,`Pedigree`.`Horse` AS `Horse`,`Pedigree`.`FatherId` AS `FatherId`,`ForGroup`.`FatherId` AS `GroupId`,`ForGroup`.`Horse` AS `GroupName`,ifnull(`ForGroup`.`Child`,0) + 1 AS `Power` from (`Pedigree` left join `ForGroup` on(`Pedigree`.`Id` = `ForGroup`.`FatherId`)) where `Pedigree`.`FatherId` is not null;
 
-/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
-/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
+-- --------------------------------------------------------
+
+--
+-- ビュー用の構造 `ForNode2`
+--
+DROP TABLE IF EXISTS `ForNode2`;
+
+CREATE ALGORITHM=UNDEFINED DEFINER=`xs386482_u10bei`@`localhost` SQL SECURITY DEFINER VIEW `ForNode2` AS with recursive ancestor(`Depth`,`Id`,`FatherId`,`GroupName`,`Power`) as (select 0 AS `Depth`,`ForNode`.`Id` AS `Id`,`ForNode`.`FatherId` AS `FatherId`,`ForNode`.`GroupName` AS `GroupName`,`ForNode`.`Power` AS `Power` from `ForNode` where `ForNode`.`Id` = 18 or `ForNode`.`Id` = 133 or `ForNode`.`Id` = 157 union all select `ancestor`.`Depth` + 1 AS `ancestor.Depth + 1`,`ForNode`.`Id` AS `Id`,`ForNode`.`FatherId` AS `FatherId`,case when `ForNode`.`Power` = 1 then `ancestor`.`GroupName` when `ancestor`.`Power` - 1 > `ForNode`.`Power` then `ancestor`.`GroupName` else `ForNode`.`GroupName` end,case when `ancestor`.`Power` - 1 > `ForNode`.`Power` then `ancestor`.`Power` - 1 else `ForNode`.`Power` end from (`ancestor` join `ForNode`) where `ancestor`.`Id` = `ForNode`.`FatherId`)select `ancestor`.`Id` AS `Id`,`ancestor`.`GroupName` AS `GroupName` from `ancestor` order by `ancestor`.`Id`;
+
+-- --------------------------------------------------------
+
+--
+-- ビュー用の構造 `ForPower`
+--
+DROP TABLE IF EXISTS `ForPower`;
+
+CREATE ALGORITHM=UNDEFINED DEFINER=`xs386482_u10bei`@`localhost` SQL SECURITY DEFINER VIEW `ForPower` AS select `Race_Winner`.`WinnerId` AS `WinnerId`,count(`Race_Winner`.`Rtitle`) AS `Win` from `Race_Winner` group by `Race_Winner`.`WinnerId`;
+
+-- --------------------------------------------------------
+
+--
+-- ビュー用の構造 `ForStratify`
+--
+DROP TABLE IF EXISTS `ForStratify`;
+
+CREATE ALGORITHM=UNDEFINED DEFINER=`xs386482_u10bei`@`localhost` SQL SECURITY DEFINER VIEW `ForStratify` AS select `Pedigree`.`Horse` AS `From`,case when `Pedigree`.`Id` = 0 then '' else `Pedigree_1`.`Horse` end AS `To`,ifnull(`ForPower`.`Win`,0) + 1 AS `Value` from ((`Pedigree` join `Pedigree` `Pedigree_1` on(`Pedigree`.`FatherId` = `Pedigree_1`.`Id`)) left join `ForPower` on(`Pedigree`.`Id` = `ForPower`.`WinnerId`));
+
+-- --------------------------------------------------------
+
+--
+-- ビュー用の構造 `Link`
+--
+DROP TABLE IF EXISTS `Link`;
+
+CREATE ALGORITHM=UNDEFINED DEFINER=`xs386482_u10bei`@`localhost` SQL SECURITY DEFINER VIEW `Link` AS select `Pedigree`.`Horse` AS `From`,`Pedigree_1`.`Horse` AS `To`,ifnull(`ForPower`.`Win`,0) + 1 AS `Value` from ((`Pedigree` join `Pedigree` `Pedigree_1` on(`Pedigree`.`FatherId` = `Pedigree_1`.`Id`)) left join `ForPower` on(`Pedigree`.`Id` = `ForPower`.`WinnerId`)) where `Pedigree`.`Id` > 0 and `Pedigree`.`FatherId` > 0;
+
+-- --------------------------------------------------------
+
+--
+-- ビュー用の構造 `Node`
+--
+DROP TABLE IF EXISTS `Node`;
+
+CREATE ALGORITHM=UNDEFINED DEFINER=`xs386482_u10bei`@`localhost` SQL SECURITY DEFINER VIEW `Node` AS select `ForNode`.`Horse` AS `Id`,`ForNode`.`Horse` AS `Label`,`ForNode2`.`GroupName` AS `Group`,ifnull(`ForNode`.`Power`,0) + ifnull(`ForPower`.`Win`,0) AS `Value` from ((`ForNode2` left join `ForNode` on(`ForNode2`.`Id` = `ForNode`.`Id`)) left join `ForPower` on(`ForNode2`.`Id` = `ForPower`.`WinnerId`));
+
+-- --------------------------------------------------------
+
+--
+-- ビュー用の構造 `StratifyLink`
+--
+DROP TABLE IF EXISTS `StratifyLink`;
+
+CREATE ALGORITHM=UNDEFINED DEFINER=`xs386482_u10bei`@`localhost` SQL SECURITY DEFINER VIEW `StratifyLink` AS select `ForStratify`.`From` AS `From`,`ForStratify`.`To` AS `To`,`ForStratify`.`Value` AS `Value`,`ForNode`.`Power` AS `Power` from (`ForStratify` join `ForNode` on(`ForStratify`.`From` = `ForNode`.`Horse`));
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `Pedigree`
+--
+ALTER TABLE `Pedigree`
+  ADD PRIMARY KEY (`Id`),
+  ADD KEY `FatherId` (`FatherId`);
+
+--
+-- Indexes for table `Race_Winner`
+--
+ALTER TABLE `Race_Winner`
+  ADD PRIMARY KEY (`Ryear`,`Rtitle`);
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
